30088e85254c4a1b69867ff4bbe8acec9789b418
Cho-Eunhak <02069@naver.com>
2021-01-05 01:20:33 +0900

    [0105 조은학]전체적인 네비게이션 완성

---
+++ Frontend/cpu_web/src/pages/Auth/Findpw.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Findpw = () => {
+  
+    return (
+      <div>
+         비밀번호찾기 페이지입니다.
+      </div>
+    );
+  };
+  
+  export default Findpw;
---
+++ Frontend/cpu_web/src/pages/Auth/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  
+    return (
+      <div>
+          로그인페이지입니다.
+      </div>
+    );
+  };
+  
+  export default Login;
