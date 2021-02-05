8d022ac89b0ef4d2f9e6ddd59726f6274b4e8059
Cho-Eunhak <02069@naver.com>
2021-02-05 19:34:55 +0900

    [0205 은학]공지사항, 취업게시판, 자유게시판 전체적인 로직 생성

--- Frontend/cpu_web/src/pages/Main/Notice.js
+++ Frontend/cpu_web/src/pages/Main/Notice.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Board from "./Board";
 
-const Notice = () => {
-  return <div>공지사항입니다.</div>;
+const Notice = ({ match }) => {
+  if (match.params.tab === "공지사항") return <Board title={"공지사항"} />;
+  else if (match.params.tab === "취업게시판")
+    return <Board title={"취업게시판"} />;
+  else if (match.params.tab === "자유게시판")
+    return <Board title={"자유게시판"} />;
 };
 
 export default Notice;
