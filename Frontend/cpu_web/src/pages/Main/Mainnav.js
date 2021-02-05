fc72770f674b0593a61de1f2b78108913b366567
Cho-Eunhak <02069@naver.com>
2021-02-05 15:53:51 +0900

    목차 왼쪽 탭수정

--- Frontend/cpu_web/src/pages/Main/Mainnav.js
+++ Frontend/cpu_web/src/pages/Main/Mainnav.js
@@ -1,17 +1,18 @@
-import { React }  from "react";
-import { Route, Switch } from 'react-router-dom';
-import { Free, Main, Welfare, Getajob } from './';
+import { React } from "react";
+import { Route, Switch } from "react-router-dom";
+import { Free, Main, Welfare, Getajob, Notice } from "./";
 const MainNav = () => {
-	return (
-		<>
-			<Switch>
-                <Route exact path="/" component={ Main }/>
-                <Route exact path="/free" component={ Free }/>
-				<Route exact path="/getajob" component={ Getajob }/>
-                <Route exact path="/welfare" component={ Welfare }/>
-			</Switch>
-		</>
-	);
+  return (
+    <>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/notice" component={Notice} />
+        <Route exact path="/free" component={Free} />
+        <Route exact path="/getajob" component={Getajob} />
+        <Route exact path="/welfare" component={Welfare} />
+      </Switch>
+    </>
+  );
 };
 
 export default MainNav;
--- Frontend/cpu_web/src/pages/Main/index.js
+++ Frontend/cpu_web/src/pages/Main/index.js
@@ -1,5 +1,6 @@
-export {default as Main} from './Main';
-export {default as Getajob} from './Getajob';
-export {default as Free} from './Free';
-export {default as Welfare} from './Welfare';
-export {default as MainLayout} from './MainLayout';
+export { default as Main } from "./Main";
+export { default as Getajob } from "./Getajob";
+export { default as Free } from "./Free";
+export { default as Notice } from "./Notice";
+export { default as Welfare } from "./Welfare";
+export { default as MainLayout } from "./MainLayout";
