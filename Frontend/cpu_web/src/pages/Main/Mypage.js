317826997e6605be1616c377f3a5cac181fffd0d
kyuri0222 <kuri222@naver.com>
2021-02-06 18:03:11 +0900

    update

--- Frontend/cpu_web/src/pages/Main/Mypage.js
+++ Frontend/cpu_web/src/pages/Main/Mypage.js
@@ -4,7 +4,6 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import CardMedia from '@material-ui/core/CardMedia';
-import './Mypage.css';
 
 const useStyles = makeStyles({
     root: {
