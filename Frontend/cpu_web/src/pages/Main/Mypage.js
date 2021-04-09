b649c7f082be8cda7107dea6f7a4937b1c7fcb8d
kyuri0222 <kuri222@naver.com>
2021-04-10 02:21:54 +0900

    Update Mypage.js

--- Frontend/cpu_web/src/pages/Main/Mypage.js
+++ Frontend/cpu_web/src/pages/Main/Mypage.js
@@ -1,18 +1,49 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import CardMedia from '@material-ui/core/CardMedia';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+import umbrella from "./umbrella.png";
+import charger from "./charger.png";
+import book from "./book.png";
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const tutorialSteps = [
+  {
+    label: '우산',
+    imgPath:
+      umbrella,
+  },
+  {
+    label: '충전기',
+    imgPath:
+      charger,
+  },
+  {
+    label: '책',
+    imgPath:
+      book,
+  }
+];
 
 const useStyles = makeStyles({
     root: {
-      minWidth: 275,
+      minWidth: 275,      
     },
     bullet: {
       display: 'inline-block',
       margin: '0 2px',
       transform: 'scale(0.8)',
+      
     },
     title: {
       fontSize: 14,
@@ -22,11 +53,47 @@ const useStyles = makeStyles({
     },
   });
 
+  const useStyles2 = makeStyles((theme) => ({
+    root: {
+      maxWidth: 400,
+      flexGrow: 1,
+    },
+    header: {
+      display: 'flex',
+      alignItems: 'center',
+      height: 50,
+      paddingLeft: theme.spacing(4),
+      backgroundColor: theme.palette.background.default,
+    },
+    img: {
+      height: 255,
+      display: 'block',
+      maxWidth: 400,
+      overflow: 'hidden',
+      width: '100%',
+    },
+  }));
 
 const Mypage = () => {
+  const classes2 = useStyles2();
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = tutorialSteps.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
 const classes = useStyles();
   const bull = <span className={classes.bullet}>•</span>;
-
   return (
     <div>
         <Card className={classes.root}>
@@ -36,14 +103,10 @@ const classes = useStyles();
         </Typography>
         </CardContent>
     </Card>
-    <br />
+    <br />    
     <Card className={classes.root}>
 
-        <CardMedia
-          className={classes.media}
-          image="https://blog.kakaocdn.net/dn/0Goax/btqBWBR99nh/7QikF7IKRyPx57hP78x3Wk/img.jpg"
-          title="My Image"
-        />
+      
 
 
         <CardContent>
@@ -59,19 +122,57 @@ const classes = useStyles();
             전공  :  데이터테크놀로지
         </Typography>
         </CardContent>
+
     </Card>
+
     <br />
+
     <Card className={classes.root}>
         <CardContent>
-        <Typography variant="body2" component="p">
-            복지 현황
+        <Typography variant="h5" component="h2">
+            복지현황
         </Typography>
         </CardContent>
+        <Paper square elevation={0} className={classes2.header}>
+        <Typography>{tutorialSteps[activeStep].label}</Typography>
+        </Paper>
+        <AutoPlaySwipeableViews
+          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+          index={activeStep}
+          onChangeIndex={handleStepChange}
+          enableMouseEvents
+        >
+          {tutorialSteps.map((step, index) => (
+            <div key={step.label}>
+              {Math.abs(activeStep - index) <= 2 ? (
+                <img className={classes2.img} src={step.imgPath} alt={step.label} />
+              ) : null}
+            </div>
+          ))}
+        </AutoPlaySwipeableViews>
+
+        <MobileStepper
+          steps={maxSteps}
+          position="static"
+          variant="text"
+          activeStep={activeStep}
+          nextButton={
+            <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+              Next
+              {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+            </Button>
+          }
+          backButton={
+            <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+              {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+              Back
+            </Button>
+          }
+        />
+        
     </Card>
     </div>
   );
 };
 
 export default Mypage;
-
-
