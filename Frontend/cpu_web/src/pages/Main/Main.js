9b3080a8653a28b56237094d4884ff03ac5ce06c
DOJIN <djyou128@gmail.com>
2021-04-10 12:34:40 +0900

    add container for calendar and notice thumnail in main

--- Frontend/cpu_web/src/pages/Main/Main.js
+++ Frontend/cpu_web/src/pages/Main/Main.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import Calendar from './Calendar';
 import NoticeThumnail from './NoticeThumnail';
 
@@ -8,10 +9,34 @@ const useStyles = makeStyles((theme) => ({
 	root: {
 		flexGrow: 1,
 	},
-	paper: {
-		padding: theme.spacing(2),
+	CaleandarHeader: {
+		backgroundColor: '#F8D67F',
 		textAlign: 'center',
-		color: theme.palette.text.secondary,
+		borderRadius: '0.3em',
+		position: 'relative',
+		zIndex: 1,
+		top: '0.2em',
+		padding: '0.1em',
+	},
+	CaleandarContainer: {
+		border: '0.2em solid #F8D67F',
+		borderTop: '0',
+		borderRadius: '0 0 0.5em 0.5em',
+	},
+	NoticeHeader: {
+		justifyItems: 'center',
+		backgroundColor: '#F8D67F',
+		textAlign: 'center',
+		borderRadius: '0.3em',
+		position: 'relative',
+		zIndex: 1,
+		top: '1em',
+		padding: '0.1em',
+	},
+	NoticeContainer: {
+		paddingTop: '2em',
+		border: '0.2em solid #F8D67F',
+		borderRadius: '0 0 0.5em 0.5em',
 	},
 }));
 
@@ -20,15 +45,38 @@ export default function Main() {
 
 	return (
 		<div className={classes.root}>
-			<Grid container spacing={2}>
-				<Grid item xs={12}>
-					<Calendar></Calendar>
+			<Grid container justify={'center'} spacing={3}>
+				<Grid item xs={10}>
+					<Typography variant='h4' className={classes.CaleandarHeader}>
+						일정
+					</Typography>
+					<Typography className={classes.CaleandarContainer}>
+						<Calendar></Calendar>
+					</Typography>
 				</Grid>
-				<Grid item xs={6}>
-					<NoticeThumnail></NoticeThumnail>
+				<Grid container item xs={6} justify='center'>
+					<Grid item xs={6}>
+						<Typography variant='h5' className={classes.NoticeHeader}>
+							공지사항
+						</Typography>
+					</Grid>
+					<Grid item xs={12}>
+						<Typography className={classes.NoticeContainer}>
+							<NoticeThumnail />
+						</Typography>
+					</Grid>
 				</Grid>
-				<Grid item xs={6}>
-					<NoticeThumnail></NoticeThumnail>
+				<Grid container item xs={6} justify='center'>
+					<Grid item xs={6}>
+						<Typography variant='h5' className={classes.NoticeHeader}>
+							공지사항
+						</Typography>
+					</Grid>
+					<Grid item xs={12}>
+						<Typography className={classes.NoticeContainer}>
+							<NoticeThumnail />
+						</Typography>
+					</Grid>
 				</Grid>
 			</Grid>
 		</div>
---
+++ Frontend/cpu_web/src/pages/Main/ThumnailCard.js
@@ -0,0 +1,25 @@
+// import * as React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import { Card, CardContent, CardHeader } from '@material-ui/core';
+
+// const useStyles = makeStyles({
+// 	root: {
+// 		minWidth: 275,
+// 	},
+// 	cardheader: {
+// 		backgroundColor: 'F8D67F',
+// 	},
+// });
+
+// export default function Main() {
+// 	const classes = useStyles();
+
+// 	return (
+// 		<Card className={classes.root}>
+// 			<CardHeader title='일정' />
+// 			<CardContent>
+// 				<Calendar></Calendar>
+// 			</CardContent>
+// 		</Card>
+// 	);
+// }
