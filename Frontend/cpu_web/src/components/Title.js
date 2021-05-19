7c10a5f3bc96547bc19763b53e2d5b9eda53e987
Cho-Eunhak <02069@naver.com>
2021-05-19 17:06:37 +0900

    타이틀 css 수정

---
+++ Frontend/cpu_web/src/components/Title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Title.css'
+const Title = () => {
+  
+    return (
+    <div class="titlebox">
+        <div class="title">
+            공지사항
+        </div>
+      </div>
+    );
+  };
+  
+  export default Title;
