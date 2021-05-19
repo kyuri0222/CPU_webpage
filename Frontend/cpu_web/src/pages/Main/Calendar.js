5630262dc00a4d51a3d3481505b6b65662d51845
DOJIN <djyou128@gmail.com>
2021-05-19 18:28:43 +0900

    update calendar

--- Frontend/cpu_web/src/pages/Main/Calendar.js
+++ Frontend/cpu_web/src/pages/Main/Calendar.js
@@ -1,10 +1,21 @@
 import * as React from 'react';
 import Paper from '@material-ui/core/Paper';
-import { ViewState } from '@devexpress/dx-react-scheduler';
+import Grid from '@material-ui/core/Grid';
+import {
+	ViewState,
+	EditingState,
+	IntegratedEditing,
+} from '@devexpress/dx-react-scheduler';
 import {
 	Scheduler,
 	MonthView,
 	Appointments,
+	AppointmentForm,
+	AppointmentTooltip,
+	ConfirmationDialog,
+	Toolbar,
+	DateNavigator,
+	TodayButton,
 } from '@devexpress/dx-react-scheduler-material-ui';
 import appointmentsData from './appointments/Appointment';
 
@@ -17,9 +28,33 @@ export default class Calendar extends React.PureComponent {
 			currentViewName: 'Month',
 		};
 
+		this.commitChanges = this.commitChanges.bind(this);
+
 		this.currentViewNameChange = (e) => {
 			this.setState({ currentViewName: e.target.value });
 		};
+	}
+
+	commitChanges({ added, changed, deleted }) {
+		this.setState((state) => {
+			let { data } = state;
+			if (added) {
+				const startingAddedId =
+					data.length > 0 ? data[data.length - 1].id + 1 : 0;
+				data = [...data, { id: startingAddedId, ...added }];
+			}
+			if (changed) {
+				data = data.map((appointment) =>
+					changed[appointment.id]
+						? { ...appointment, ...changed[appointment.id] }
+						: appointment
+				);
+			}
+			if (deleted !== undefined) {
+				data = data.filter((appointment) => appointment.id !== deleted);
+			}
+			return { data };
+		});
 	}
 
 	render() {
@@ -33,9 +68,18 @@ export default class Calendar extends React.PureComponent {
 							defaultCurrentDate={new Date()}
 							currentViewName={currentViewName}
 						/>
+						<EditingState onCommitChanges={this.commitChanges} />
+						<IntegratedEditing />
 						<MonthView />
+						<Toolbar />
+						<DateNavigator />
+						<TodayButton />
+						<ConfirmationDialog />
 						<Appointments />
+						<AppointmentTooltip showOpenButton showDeleteButton />
+						<AppointmentForm />
 					</Scheduler>
+					<Grid container justify={'center'} spacing={3}></Grid>
 				</Paper>
 			</React.Fragment>
 		);
--- Frontend/cpu_web/src/pages/Main/NoticeThumnail.js
+++ Frontend/cpu_web/src/pages/Main/NoticeThumnail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -11,18 +12,25 @@ const useStyles = makeStyles({
 	table: {
 		minWidth: 100,
 	},
+	cellContainer: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		// borderBottomColor: 'rgb(224, 224, 224)',
+		// borderBottomStyle: 'solid',
+		borderBottom: '1px solid rgb(224, 224, 224)',
+	},
 });
 
-function createData(title, date) {
-	return { title, date };
+function createData(id, title, date) {
+	return { id, title, date };
 }
 
 const rows = [
-	createData('공지사항5', new Date('2021-04-09')),
-	createData('공지사항4', new Date('2021-04-05')),
-	createData('공지사항3', new Date('2021-04-03')),
-	createData('공지사항2', new Date('2021-04-01')),
-	createData('공지사항1', new Date('2021-03-29')),
+	createData(1, '공지사항5', new Date('2021-04-09')),
+	createData(2, '공지사항4', new Date('2021-04-05')),
+	createData(3, '공지사항3', new Date('2021-04-03')),
+	createData(4, '공지사항2', new Date('2021-04-01')),
+	createData(5, '공지사항1', new Date('2021-03-29')),
 ];
 
 export default function NoticeThumnail() {
@@ -34,18 +42,26 @@ export default function NoticeThumnail() {
 				<TableBody>
 					{rows.map((row) => (
 						<TableRow key={row.title}>
-							<TableCell component='th' scope='row'>
-								{row.title}
-							</TableCell>
-							<TableCell align='right'>{`${row.date.getFullYear()}-${
-								row.date.getMonth() + 1 > 9
-									? row.date.getMonth() + 1
-									: '0' + (row.date.getMonth() + 1)
-							}-${
-								row.date.getDay() > 9
-									? row.date.getDay()
-									: '0' + row.date.getDay()
-							}`}</TableCell>
+							<Link
+								to={`/Announce/detail/${row.id}`}
+								class={classes.cellContainer}
+							>
+								<TableCell style={{ borderBottom: 'None' }} scope='row'>
+									{row.title}
+								</TableCell>
+								<TableCell
+									style={{ borderBottom: 'None' }}
+									align='right'
+								>{`${row.date.getFullYear()}-${
+									row.date.getMonth() + 1 > 9
+										? row.date.getMonth() + 1
+										: '0' + (row.date.getMonth() + 1)
+								}-${
+									row.date.getDay() > 9
+										? row.date.getDay()
+										: '0' + row.date.getDay()
+								}`}</TableCell>
+							</Link>
 						</TableRow>
 					))}
 				</TableBody>
