1cc1da42955ae8231f036f93362289ee03edc73b
Cho-Eunhak <02069@naver.com>
2021-05-19 15:16:06 +0900

    refactor: 공지사항, 자유게시판, 취업게시판 파일구조변경

---
+++ Frontend/cpu_web/src/pages/Main/Free/Free.js
@@ -0,0 +1,145 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link, useParams } from "react-router-dom";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Title from "../Title";
+import PaginationItem from "@material-ui/lab/PaginationItem";
+import Pagination from "@material-ui/lab/Pagination";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+function createData(id, title, name, date) {
+  return { id, title, name, date };
+}
+
+const rows = [
+  createData("1", "제목 테스트입니다", "조은학", "20200501"),
+  createData("2", "제목 테스트입니다", "조은학", "20200501"),
+  createData("3", "제목 테스트입니다", "조은학", "20200501"),
+  createData("4", "제목 테스트입니다", "조은학", "20200501"),
+  createData("5", "제목 테스트입니다", "조은학", "20200501"),
+  createData("6", "제목 테스트입니다", "조은학", "20200501"),
+  createData("7", "제목 테스트입니다", "조은학", "20200501"),
+  createData("8", "제목 테스트입니다", "조은학", "20200501"),
+  createData("9", "제목 테스트입니다", "조은학", "20200501"),
+  createData("10", "제목 테스트입니다", "조은학", "20200501"),
+  createData("11", "제목 테스트입니다", "조은학", "20200501"),
+  createData("12", "제목 테스트입니다", "조은학", "20200501"),
+  createData("13", "제목 테스트입니다", "조은학", "20200501"),
+  createData("14", "제목 테스트입니다", "조은학", "20200501"),
+];
+
+function preventDefault(event) {
+  event.preventDefault();
+}
+
+const useStyles = makeStyles((theme) => ({
+  seeMore: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+export default function Free({ title }) {
+  const USER_PATH = `/Free/detail`;
+  const ROWS_PER_PAGE = 10;
+  const { pageNumber = 1 } = useParams();
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Title>자유게시판</Title>
+      <Link to={`/Free/create`}>
+        <Button color="primary" variant="contained">
+          작성하기
+        </Button>
+      </Link>
+      <Table className={classes.table} size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center" width="10%">
+              <Typography component={"span"} variant="subtitle2">
+                번호
+              </Typography>
+            </TableCell>
+            <TableCell align="center" width="50%">
+              제목
+            </TableCell>
+            <TableCell align="center" width="20%">
+              작성자
+            </TableCell>
+            <TableCell align="center" width="20%">
+              작성일
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {(ROWS_PER_PAGE > 0
+            ? rows.slice(
+                (Number(pageNumber) - 1) * ROWS_PER_PAGE,
+                (Number(pageNumber) - 1) * ROWS_PER_PAGE + ROWS_PER_PAGE
+              )
+            : rows
+          ).map((row) => {
+            return (
+              <TableRow key={row.id}>
+                <TableCell
+                  component={Link}
+                  to={`/Free/detail/${row.id}`}
+                  style={{ textDecoration: "none" }}
+                  align="center"
+                  width="10%"
+                >
+                  {row.id}
+                </TableCell>
+                <TableCell
+                  component={Link}
+                  to={`/Free/detail/${row.id}`}
+                  style={{ textDecoration: "none" }}
+                  width="50%"
+                >
+                  {row.title}
+                </TableCell>
+                <TableCell align="center" width="20%">
+                  {row.name}
+                </TableCell>
+                <TableCell align="center" width="20%">
+                  {row.date}
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+
+      <Box
+        display="flex"
+        justifyContent="flex-end"
+        flex={1}
+        padding={1}
+        paddingRight={10}
+      >
+        <Pagination
+          page={Number(pageNumber)}
+          count={Math.ceil(rows.length / ROWS_PER_PAGE)}
+          shape="rounded"
+          color="primary"
+          showFirstButton
+          showLastButton
+          boundaryCount={2}
+          renderItem={(item) => (
+            <PaginationItem
+              type={"start-ellipsis"}
+              component={Link}
+              selected
+              to={`${USER_PATH}/${item.page}`}
+              {...item}
+            />
+          )}
+        />
+      </Box>
+    </React.Fragment>
+  );
+}
---
+++ Frontend/cpu_web/src/pages/Main/Free/FreeCreate.js
@@ -0,0 +1,69 @@
+import Editor from "../QuillEditor";
+import React, { useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+const NoticeCreate = ({ match }) => {
+  const { team } = match.params;
+  const [desc, setDesc] = useState("");
+  function onEditorChange(value) {
+    setDesc(value);
+  }
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      padding: theme.spacing(2),
+      display: "flex",
+      overflow: "auto",
+      flexDirection: "column",
+    },
+    fixedHeight: {
+      height: 240,
+    },
+  }));
+  const classes = useStyles();
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+  const [title, settitle] = useState();
+  const onTitleChange = (e) => {
+    settitle(e.target.value);
+  };
+  return (
+    <div>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <div>자유게시판</div>
+            <TextField
+              id="outlined-full-width"
+              label="제목"
+              placeholder="제목을 입력하세요"
+              fullWidth
+              margin="normal"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+              value={title}
+              onChange={onTitleChange}
+            />
+            <Editor value={desc} onChange={onEditorChange} />
+            <Button
+              component={Link}
+              to={"/Notice"}
+              variant="contained"
+              color="primary"
+            >
+              저장하기
+            </Button>
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default NoticeCreate;
